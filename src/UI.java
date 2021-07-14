ae7bcba6cd993dfd00f8c84ac61392f3cb79f595
Jordan Bravo <jbravo0423@gmail.com>
2021-07-13 21:17:58 -0400

    Created the main game screen. WIP.

--- src/UI.java
+++ src/UI.java
@@ -1,17 +1,15 @@
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
 public class UI{
 
     JFrame gameWindow;
-    JPanel startQuitPanel, titlePanel, dungeonTextPanel, choicePanel;
+    JPanel startQuitPanel, titlePanel;
     JLabel titleLabel;
-    JButton startButton, quitButton, choiceAttack, choicePotion, choiceFlee;
-    JTextArea dungeonText;
+    JButton startButton, quitButton;
     Font titleFont = new Font("Serif", Font.ITALIC, 90);
-    Font regFont = new Font("Serif", Font.PLAIN, 25);
+    Font startFont = new Font("Serif", Font.PLAIN, 25);
+    Font quitFont = new Font("Serif", Font.PLAIN, 25);
     Container container;
 
     // START MENU
@@ -46,79 +44,13 @@ public class UI{
         startQuitPanel.setBackground(Color.black);
         startButton.setBackground(Color.black);
         startButton.setForeground(Color.white);
-        startButton.setFont(regFont);
-        startButton.addActionListener(new ActionHandler());
+        startButton.setFont(startFont);
         quitButton.setBackground(Color.black);
         quitButton.setForeground(Color.white);
-        quitButton.setFont(regFont);
-        quitButton.addActionListener(new QuitHandler());
+        quitButton.setFont(quitFont);
         startQuitPanel.add(startButton);
         startQuitPanel.add(quitButton);
         container.add(startQuitPanel);
 
-    }
-
-    // MAIN GAME SCREEN
-    public void gameScreen(){
-        GridLayout layout = new GridLayout(4,1);
-        dungeonTextPanel = new JPanel();
-        dungeonText = new JTextArea("This is the main text area. Placeholder text @@@@@@@@@@@@@@@@@@@@@@@@");
-        choicePanel = new JPanel();
-        choiceAttack = new JButton("Attack");
-        choicePotion = new JButton("Potion");
-        choiceFlee = new JButton("Flee");
-
-        // MOVE FROM MAIN SCREEN TO MAIN SCREEN
-        titlePanel.setVisible(false);
-        startQuitPanel.setVisible(false);
-
-        // MAIN TEXT PANEL FOR GAME
-        dungeonTextPanel.setBounds(100, 100, 600, 300);
-        dungeonTextPanel.setBackground(Color.black);
-        dungeonText.setBounds(0, 100, 450, 300);
-        dungeonText.setBackground(Color.black);
-        dungeonText.setForeground(Color.white);
-        dungeonText.setFont(regFont);
-        dungeonText.setLineWrap(true);
-        dungeonTextPanel.add(dungeonText);
-        container.add(dungeonTextPanel);
-
-        // CHOICE BUTTONS AND PANEL
-        layout.setVgap(5);
-        choicePanel.setBounds(300, 400, 200, 150);
-        choicePanel.setBackground(Color.black);
-        choicePanel.setLayout(layout);
-        container.add(choicePanel);
-
-        choiceAttack.setBackground(Color.black);
-        choiceAttack.setForeground(Color.white);
-        choiceAttack.setFont(regFont);
-        choicePanel.add(choiceAttack);
-        choicePotion.setBackground(Color.black);
-        choicePotion.setForeground(Color.white);
-        choicePotion.setFont(regFont);
-        choicePanel.add(choicePotion);
-        choiceFlee.setBackground(Color.black);
-        choiceFlee.setForeground(Color.white);
-        choiceFlee.setFont(regFont);
-        choicePanel.add(choiceFlee);
-    }
-
-    // START BUTTON ACTION
-    public class ActionHandler implements ActionListener{
-
-        @Override
-        public void actionPerformed(ActionEvent e) {
-            gameScreen();
-        }
-    }
-
-    // QUIT BUTTON ACTION
-    public static class QuitHandler implements ActionListener{
-
-        @Override
-        public void actionPerformed(ActionEvent e) {
-            System.exit(0);
-        }
     }
 }
